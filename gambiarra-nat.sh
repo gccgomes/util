129467afd889cf9a42d59f30b966bae27b715b02
Geraldo Gomes <gccgomes@users.noreply.github.com>
2022-04-26 09:12:23 -0300

    Create gambiarra-nat.sh

---
+++ gambiarra-nat.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# simple socat usages
+#open an udp port and forward to another ip
+socat UDP4-RECVFROM:53,fork UDP4-SENDTO:192.168.0.1:53
+# open an tcp port and forward to another ip
+socat TCP-LISTEN:443,fork TCP:192.168.0.1:443
