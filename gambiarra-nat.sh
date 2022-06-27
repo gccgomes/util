acd4c3d5d521298ee8c859866fd09067012f7d68
Geraldo Gomes <gccgomes@users.noreply.github.com>
2022-06-27 08:16:17 -0300

    Update gambiarra-nat.sh

--- gambiarra-nat.sh
+++ gambiarra-nat.sh
@@ -4,3 +4,7 @@
 socat UDP4-RECVFROM:53,fork UDP4-SENDTO:192.168.0.1:53
 # open an tcp port and forward to another ip
 socat TCP-LISTEN:443,fork TCP:192.168.0.1:443
+
+
+# du command for hidden files
+du -hs .[^.]*
